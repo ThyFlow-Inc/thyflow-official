23d0229e9e89bf4977392bb6a82cae1592ee5119
adetyaz <tayo.lasisi@yahoo.com>
2021-07-19 15:43:39 +0100

    ..

--- components/subscribe-form/subscribe-form.jsx
+++ components/subscribe-form/subscribe-form.jsx
@@ -1,4 +1,39 @@
-const SubscribeForm =()=>(
+import React, { useRef, useState } from 'react';
+
+const SubscribeForm =()=>{
+     // 1. Create a reference to the input so we can fetch/clear it's value.
+  const inputEl = useRef(null);
+  // 2. Hold a message in state to handle the response from our API.
+  const [message, setMessage] = useState('');
+
+  const subscribe = async (e) => {
+    e.preventDefault();
+
+    // 3. Send a request to our API with the user's email address.
+    const res = await fetch('/api/subscribe', {
+      body: JSON.stringify({
+        email: inputEl.current.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    });
+
+    const { error } = await res.json();
+
+    if (error) {
+      // 4. If there was an error, update the message in state.
+      setMessage(error);
+
+      return;
+    }
+
+    // 5. Clear the input value and show a success message.
+    inputEl.current.value = '';
+    setMessage('Success! 🎉 You are now subscribed to the newsletter.');
+  };
+    return(
     <>
     <div className="subscribe">
         <h1 className="subscribe__form-title">
@@ -7,10 +42,20 @@ const SubscribeForm =()=>(
             <h2 className="subscribe__heading-secondary">
                 Subscribe to our weekly newsletter to get tips sent to your inbox.
             </h2>
-            <form action="" className="subscribe__form">
-                <input type="text" className="subscribe__input" placeholder="Your Email here"/>
+            <form action="" className="subscribe__form" onSubmit={subscribe}>
+                <input type="text" 
+                    className="subscribe__input" 
+                    placeholder="Your Email here"
+                    ref={inputEl}
+                    required
+                    type="email"
+                />
                 <div>
-            
+                <div className="message">
+                    {message
+                    ? message
+                    : ''}
+                </div>
                 <button className="btn btn--primary pad">
                    <span className="btn__text">Subscribe</span> 
                 </button>
@@ -18,6 +63,7 @@ const SubscribeForm =()=>(
             </form>
             </div>
     </>
-);
+)
+}
 
 export default SubscribeForm;
