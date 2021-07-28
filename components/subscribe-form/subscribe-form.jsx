11d018d0ccb6f2fa29779c46b73e6aee3aed767a
adetyaz <tayo.lasisi@yahoo.com>
2021-07-28 16:49:20 +0100

    subscribe page

--- components/subscribe-form/subscribe-form.jsx
+++ components/subscribe-form/subscribe-form.jsx
@@ -1,42 +1,42 @@
 import React, { useRef, useState } from 'react';
 
 const SubscribeForm =()=>{
-     // 1. Create a reference to the input so we can fetch/clear it's value.
-  const inputEl = useRef(null);
-  const inputName = useRef(null);
-  // 2. Hold a message in state to handle the response from our API.
-  const [message, setMessage] = useState('');
+  //    // 1. Create a reference to the input so we can fetch/clear it's value.
+  // const inputEl = useRef(null);
+  // const inputName = useRef(null);
+  // // 2. Hold a message in state to handle the response from our API.
+  // const [message, setMessage] = useState('');
 
-  const subscribe = async (e) => {
-    e.preventDefault();
+  // const subscribe = async (e) => {
+  //   e.preventDefault();
 
-    // 3. Send a request to our API with the user's email address.
-    const res = await fetch('/api/subscribe', {
-      body: JSON.stringify({
-        email: inputEl.current.value,
-        name: inputName.current.value
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    });
+  //   // 3. Send a request to our API with the user's email address.
+  //   const res = await fetch('/api/subscribe', {
+  //     body: JSON.stringify({
+  //       email: inputEl.current.value,
+  //       name: inputName.current.value
+  //     }),
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     method: 'POST'
+  //   });
 
-    const { error } = await res.json();
+  //   const { error } = await res.json();
 
-    if (error) {
-      // 4. If there was an error, update the message in state.
-      setMessage(error);
+  //   if (error) {
+  //     // 4. If there was an error, update the message in state.
+  //     setMessage(error);
 
-      return;
-    }
+  //     return;
+  //   }
 
-    // 5. Clear the input value and show a success message.
-    inputEl.current.value = '';
-    inputName.current.value = '';
+  //   // 5. Clear the input value and show a success message.
+  //   inputEl.current.value = '';
+  //   inputName.current.value = '';
     
-    setMessage('Success! 🎉 You are now subscribed to the newsletter.');
-  };
+  //   setMessage('Success! 🎉 You are now subscribed to the newsletter.');
+  // };
     return(
     <>
     <div className="subscribe">
